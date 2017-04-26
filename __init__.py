b1659651afdb5fb0e3790ff0474c1c13da580f3a
Al Avery <averyab0@sewanee.edu>
2017-04-26 01:43:11 -0500

    Test commit from computer

--- __init__.py
+++ __init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello, I changed this from my computer!"
+    return "Hello, I really did change this from my computer!"
 if __name__ == "__main__":
     app.run()
