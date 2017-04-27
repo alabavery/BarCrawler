38cf12d59c8a56de9b607ff2a0a92c22e6d0e288
Al Avery <averyab0@sewanee.edu>
2017-04-26 19:34:24 -0500

    Revert "Revert "Ignore, just getting rid of staging""
    This reverts commit 28ec00ede234070f9d9755f4dbfccf25bacca5b1.
    
    undoing commit 28ec00ede234070f9d9755f4dbfccf25bacca5b1

--- __init__.py
+++ __init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
-# app = Flask(__name__)
-# @app.route("/")
-# def hello():
-#     return "Hello, I really did change this from my computer!"
-
-import views
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello, I really did change this from my computer!"
 
 if __name__ == "__main__":
-    views.app.run()
+    app.run()
