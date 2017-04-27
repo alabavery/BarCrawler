428f7fd50a93364d4ad9f7751e3d6172bd85b79d
Al Avery <averyab0@sewanee.edu>
2017-04-26 20:12:29 -0500

    Use render_template and add static content

--- __init__.py
+++ __init__.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
+
+@app.route("/index")
 @app.route("/")
 def hello():
-    return "Hello, I really did change this from my computer!"
+    return render_template('base.html',
+                  title='BarCrawler | Find Music in Chicago Bars',
+                  )
+
+
+
+
 if __name__ == "__main__":
     app.run()
