a200890850522b55b87a833d9557b27c84ca572b
Al Avery <averyab0@sewanee.edu>
2017-04-26 19:39:04 -0500

    Revert "Testing views.py"
    This reverts commit b0c9bb8188ec7f7581c1829d3585209f147f78db.

--- __init__.py
+++ __init__.py
@@ -1,10 +1,7 @@
 from flask import Flask
-# app = Flask(__name__)
-# @app.route("/")
-# def hello():
-#     return "Hello, I really did change this from my computer!"
-
-import views
-
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello, I really did change this from my computer!"
 if __name__ == "__main__":
-    views.app.run()
+    app.run()
