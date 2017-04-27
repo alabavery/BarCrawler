e28cdad85d0e8949b8d42c6a5a53c2b05957c454
Al Avery <averyab0@sewanee.edu>
2017-04-27 16:31:17 -0500

    Changes to html

--- __init__.py
+++ __init__.py
@@ -5,12 +5,17 @@ app = Flask(__name__)
 
 @app.route("/index")
 @app.route("/")
-def hello():
-    return render_template('base.html',
-                  title='BarCrawler | Find Music in Chicago Bars',
-                  )
+def index():
+    return render_template('index.html',
+    					title='ChiBarCrawler | Find Music in Chicago Bars'
+                  		)
 
 
+@app.route("/shows")
+def shows():
+	return render_template('shows.html',
+						title='Shows | ChiBarCrawler'
+				)
 
 
 if __name__ == "__main__":
