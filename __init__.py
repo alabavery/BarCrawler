68aa4554fb02fc5e2ab892d86204f878bfad0424
Al Avery <averyab0@sewanee.edu>
2017-05-04 11:33:17 -0500

    First commit of new json integration

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from json import loads as json_loads
 
 app = Flask(__name__)
 
@@ -11,12 +12,24 @@ def index():
                   		)
 
 
+json_file = open("json_file.json", 'r')
+show_data = json_loads(json_file.read())
+json_file.close()
 @app.route("/shows")
 def shows():
 	return render_template('shows.html',
-						title='Shows | ChiBarCrawler'
-				)
+						title='Shows | ChiBarCrawler',
+						show_data=show_data
+						)
+
+
+@app.route("/spotify-play")
+def spotify_play(song_id):
+	return render_template('spotify_play_button.html',
+						title='Spotify Play',
+						song_id=song_id
+						)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
