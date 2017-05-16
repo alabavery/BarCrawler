2f92a08d993b64d4c66bd76fd1c6d761ad620312
Al Avery <averyab0@sewanee.edu>
2017-05-16 11:05:17 -0500

    Insertion of band image in expanded show

--- static/js/shows.js
+++ static/js/shows.js
@@ -7,10 +7,12 @@ $(document).ready(function(){
 			artist_id: $(that).attr('artist-id')
 		}, function(artist_info) {
 			var spotify_play_uri = artist_info['spotify_track_id']
+			var spotify_image = artist_info['image']
 
 			if(spotify_play_uri) { // possible that we could not find the artist on spotify and will return null
 				$(that).find('.spotify-play').append('<iframe src="https://open.spotify.com/embed?uri=spotify:\
 					track:'+spotify_play_uri+'"</iframe>');
+				$(that).find('.band-image').append('<img src='+spotify_image+'>');
 			} else {
 				alert("Sorry we either couldn't find or couldn't verify that artist on Spotify.");
 			}
