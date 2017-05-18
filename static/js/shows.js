6bef9617ab33e71485018c817c4bd132bdd6a415
Al Avery <averyab0@sewanee.edu>
2017-05-18 00:19:37 -0500

    Change to no expansion approach

--- static/js/shows.js
+++ static/js/shows.js
@@ -1,21 +1,16 @@
 $(document).ready(function(){
-	$('#show-container').on('click', ".show-tile", function() {
-		$(this).children().toggle();
-		$(this).toggleClass('col-sm-3 col-sm-12');
-   
+	$('.expand-spotify').click(function() {
 		var that = this;
 		$.getJSON($SCRIPT_ROOT + '/_expand_show', {
 			artist_id: $(that).attr('artist-id')
 		}, function(artist_info) {
 			var spotify_play_uri = artist_info['spotify_track_id']
-			var spotify_image = artist_info['image']
-
 			if(spotify_play_uri) { // possible that we could not find the artist on spotify and will return null
-				$(that).find('.spotify-play').append('<iframe src="https://open.spotify.com/embed?uri=spotify:\
+				$(that).parent().append('<iframe src="https://open.spotify.com/embed?uri=spotify:\
 					track:'+spotify_play_uri+'"</iframe>');
-				$(that).find('.expanded-show-image').attr('src', spotify_image);
+				$(that).remove();
 			} else {
-				alert("Sorry we either couldn't find or couldn't verify that artist on Spotify.");
+				$(that).find('h4').text("sorry, we couldn't find this artist on spotify.");
 			}
 		});
 		return false;
