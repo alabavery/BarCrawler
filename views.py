f264ada11a42f24f93a29ea10506d2f0e2d3d08a
Al Avery <averyab0@sewanee.edu>
2017-07-15 23:24:26 -0500

    Move artist_data back to global bc needed by multiple fxns

--- views.py
+++ views.py
@@ -13,14 +13,11 @@ def index():
     return render_template('index.html',
     					title='ChiBarCrawler | Find Music in Chicago Bars'
                   		)
-
+json_file = open(config.ARTIST_FILE_PATH, 'r')
+artist_data = json_loads(json_file.read())
+json_file.close()
 
 def military_to_pretty_time(military_time):
-	# 00:30:00 = 12:30 am
-	# 11:00:00 = 11 am
-	# 13:00:00 = 1 pm
-	# 20:00:00 = 8 pm
-	# 23:00:00 = 11 pm
 	if int(military_time[3:5]) > 0:
 		minute = ":" + military_time[3:5]
 	else:
@@ -52,9 +49,6 @@ def shows():
 
 	json_file = open(config.SONGKICK_FILE_PATH, 'r')
 	show_data = json_loads(json_file.read())
-	json_file.close()
-	json_file = open(config.ARTIST_FILE_PATH, 'r')
-	artist_data = json_loads(json_file.read())
 	json_file.close()
 
 	prettify_dates_and_times(show_data)
